3beec085a46ba6469727d337eeeab76af6adf284
LucaFlorin <luca.florin.adrian@gmail.com>
2018-08-08 13:16:38 +0300

    Added js for the burger menu

--- Freelancer.js
+++ Freelancer.js
@@ -1,7 +1,17 @@
-
-
 $(document).ready(function(){
   $('.dropdown').click(function(){
-      $('.dropdown_content').append
+     /* $('.dropdown_content').css('cssText', 'visibility: visible; opacity: 1;'  );
+     $('.dropdown_content').toggleClass('dropdown_content_click');*/
+      $('.menu_line1').toggleClass('menulinev1');
+      $('.menu_line2').toggleClass('menulinev2');
+      $('.menu_line3').toggleClass('menulinev3');
+
   });
+
+
+  $('.dropdown').click(function(){ 
+      $('.dropdown_content').toggleClass('dropdown_content_click');
+   });
+
+
 });
